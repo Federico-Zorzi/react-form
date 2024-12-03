934a0439c223b0693b5ab81dca7217bdde2352c3
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-12-03 15:11:15 +0100

    new form created

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+export default function Article() {
+  return (
+    <div className="card my-3">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,8 +1,33 @@
+import Article from "../Article";
+
 export default function Main() {
   return (
     <main>
       <div className="container">
-        <p>Main</p>
+        <form>
+          {/* INPUT FOR ARTICLE TITLE */}
+          <div class="mb-3">
+            <label for="articleTitle" class="form-label">
+              Titolo articolo
+            </label>
+            <input type="text" class="form-control" id="articleTitle" />
+          </div>
+
+          {/* INPUT FOR ARTICLE CONTENT */}
+          <div class="mb-3">
+            <label for="articleContent" class="form-label">
+              Contenuto articolo
+            </label>
+            <input type="text" class="form-control" id="articleContent" />
+          </div>
+
+          {/* BUTTON FOR SUBMIT */}
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+
+        <Article></Article>
       </div>
     </main>
   );
