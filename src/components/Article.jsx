26ad1b3c5c3b7fd416d3d873805bfa0bd191ad02
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-12-03 16:20:55 +0100

    removing function added

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,17 @@
-export default function Article() {
+export default function Article({ index, title, content, deleteFunction }) {
   return (
     <div className="card my-3">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title fw-semibold">{title}</h5>
+        <p className="card-text">{content}</p>
+
+        <button
+          className="btn btn-danger ms-auto"
+          type="button"
+          onClick={() => deleteFunction(index)}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -5,56 +5,89 @@ import Article from "../Article";
 import articles from "../../assets/data/articles";
 
 export default function Main() {
-  const [articleTitleInput, setArticleTitle] = useState("");
-  const [articleContentInput, setArticleContent] = useState("");
+  const [articleTitleInput, setArticleTitleInput] = useState("");
+  const [articleContentInput, setArticleContentInput] = useState("");
+
+  const [articlesList, setArticlesList] = useState(articles);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!articleTitleInput || !articleContentInput) {
+      alert("Completa tutti i campi per creare il nuovo articolo");
+      return;
+    }
+
+    const updatedList = [
+      ...articlesList,
+      { title: articleTitleInput, content: articleContentInput },
+    ];
+    setArticlesList(updatedList);
+  };
+
+  const deleteArticle = (index) => {
+    const updatedList = articlesList.filter((article, i) => i !== index);
+
+    setArticlesList(updatedList);
   };
 
   return (
     <main>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          {/* INPUT FOR ARTICLE TITLE */}
-          <div className="mb-3">
-            <label htmlFor="articleTitle" className="form-label">
-              Titolo articolo
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="articleTitle"
-              value={articleTitleInput}
-              onChange={(e) => {
-                setArticleTitle(e.target.value);
-              }}
-            />
-          </div>
+        {/* FORM SECTION */}
+        <section className="form-section">
+          <form onSubmit={handleSubmit}>
+            {/* INPUT FOR ARTICLE TITLE */}
+            <div className="mb-3">
+              <label htmlFor="articleTitle" className="form-label">
+                Titolo articolo
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="articleTitle"
+                value={articleTitleInput}
+                onChange={(e) => {
+                  setArticleTitleInput(e.target.value);
+                }}
+              />
+            </div>
 
-          {/* INPUT FOR ARTICLE CONTENT */}
-          <div className="mb-3">
-            <label htmlFor="articleContent" className="form-label">
-              Contenuto articolo
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="articleContent"
-              value={articleContentInput}
-              onChange={(e) => {
-                setArticleContent(e.target.value);
-              }}
-            />
-          </div>
+            {/* INPUT FOR ARTICLE CONTENT */}
+            <div className="mb-3">
+              <label htmlFor="articleContent" className="form-label">
+                Contenuto articolo
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="articleContent"
+                value={articleContentInput}
+                onChange={(e) => {
+                  setArticleContentInput(e.target.value);
+                }}
+              />
+            </div>
 
-          {/* BUTTON FOR SUBMIT */}
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+            {/* BUTTON FOR SUBMIT */}
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </section>
 
-        <Article></Article>
+        {/* ARTICLE SECTION */}
+        <section className="article-section">
+          {articlesList.map((article, index) => (
+            <Article
+              key={index}
+              index={index}
+              title={article.title}
+              content={article.content}
+              deleteFunction={deleteArticle}
+            ></Article>
+          ))}
+        </section>
       </div>
     </main>
   );
